3ee400dcafc8ffd9a202141cbbea4f6375fb6f73
Charles Carpenter <carpe359@gmail.com>
2021-10-31 16:53:07 -0500

    lab_3

---
+++ src/myutil/Person.java
@@ -0,0 +1,17 @@
+package myutil;
+
+public class Person {
+    private String name;
+
+    public Person() {
+        name = "Unknown";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
